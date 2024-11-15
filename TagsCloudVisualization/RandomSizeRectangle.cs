b682a7ff406ef284a82d5b0b8a03ee3c887c2916
Dmitriy Bessarab <bessarabds@gmail.com>
2024-11-15 15:36:50 +0500

    rectangles generation

---
+++ TagsCloudVisualization/RandomSizeRectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    public class RandomSizeRectangle : IRectangleGenerator
+    {
+        public IEnumerable<Size> GenerateRectangles(int quantity)
+        {
+            var rnd = new Random();
+            for (var i = 0; i < quantity; i++)
+                yield return new Size(rnd.Next(5, 30), rnd.Next(5, 30));
+        }
+    }
+}

534e655a8d9c989f8ec9b4e1f2c68dcfcdc7ce49
Dmitriy Bessarab <bessarabds@gmail.com>
2024-11-15 15:30:30 +0500

    1st attempt

---
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagsCloudVisualization
+{
+    public class CircularCloudLayouter
+    {
+        public readonly Point Center;
+        public readonly Size Size;
+        private readonly IEnumerable<Point> _points;
+        public List<Rectangle> Rectangles { get; set; }
+
+
+        public CircularCloudLayouter(Point center)
+        {
+            if (center.X > 0 && center.Y > 0)
+                Center = center;
+            else throw new ArgumentException("Center coordinates values have to be greater than Zero");
+            Size = CountSize(center);
+            Rectangles = [];
+            _points = new ArchemedianSpiral().GeneratePoints(Center);
+        }
+
+        public CircularCloudLayouter(Point center, IEnumerable<Point> points) : this(center)
+        {
+            Center = center;
+            Size = CountSize(center);
+            Rectangles = [];
+            _points = points;
+        }
+
+        public CircularCloudLayouter(Point center, Func<Point, IEnumerable<Point>> pointGenerator) : this(center)
+        {
+            Center = center;
+            Size = CountSize(center);
+            Rectangles = [];
+            _points = pointGenerator(Center);
+        }
+
+
+        private Size CountSize(Point center)
+        {
+            var width = (center.X % 2 == 0) ? center.X * 2 + 1 : Center.X * 2;
+            var height = (center.Y % 2 == 0) ? center.Y * 2 + 1 : center.Y * 2;
+            return new Size(width, height);
+        }
+
+
+        //public Rectangle PutNextRectangle(Size rectangleSize)
+        //{
+        //    var points = _points.GetEnumerator();
+        //    points.MoveNext();
+
+        //    while (true)
+        //    {
+        //        var point = points.Current;
+        //        var supposed = new Rectangle(new Point(point.X - rectangleSize.Width / 2, point.Y - rectangleSize.Height / 2),
+        //            rectangleSize);
+        //        if (Rectangles.Count == 0)
+        //        {
+        //            Rectangles.Add(supposed);
+        //            return supposed;
+        //        }
+
+        //        if (IntersectsWithAnyOther(supposed, Rectangles))
+        //        {
+        //            points.MoveNext();
+        //            continue;
+        //        }
+        //        else
+        //        {
+        //            Rectangles.Add(supposed);
+        //        }
+
+        //        points.MoveNext();
+
+        //    }
+        //}
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            if (Rectangles.Count == 0)
+            {
+                var firstRectangle = new Rectangle(new Point(Center.X - rectangleSize.Width / 2,
+                    Center.Y - rectangleSize.Height / 2), rectangleSize);
+                Rectangles.Add(firstRectangle);
+                return firstRectangle;
+            }
+
+            foreach (var point in _points)
+            {
+                var supposed = new Rectangle(new Point(point.X - rectangleSize.Width / 2, point.Y - rectangleSize.Height / 2),
+                          rectangleSize);
+                if (IntersectsWithAnyOther(supposed, Rectangles))
+                    continue;
+                Rectangles.Add(supposed);
+                return supposed;
+            }
+            return new Rectangle();
+        }
+
+        public static bool IntersectsWithAnyOther(Rectangle supposed, List<Rectangle> others)
+        {
+            return others.Select(x => x.IntersectsWith(supposed))
+                .Any(x => x == true);
+        }
+    }
+}
---
+++ TagsCloudVisualization/CircularCloudLayouter_Should.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+using System.Reflection;
+using System.Drawing;
+
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class CircularCloudLayouter_Should
+    {
+        [TestCase(1, 2, TestName = "Odd coordinate value results in an even size value")]
+        [TestCase(2, 5, TestName = "Even coordinate value results in an odd size value")]
+        public void CircularCloudLayouter_MakeRightSizeLayout(int coordinateValue, int sizeValue)
+        {
+            var center = new Point(coordinateValue, coordinateValue);
+            var size = new Size(sizeValue, sizeValue);
+
+            var layout = new CircularCloudLayouter(center);
+
+            layout.Size.Should().BeEquivalentTo(size);
+        }
+
+        [TestCase(-1, 1, TestName = "Negative X")]
+        [TestCase(1, -1, TestName = "Negative Y")]
+        [TestCase(0, 1, TestName = "Zero X")]
+        [TestCase(1, 0, TestName = "Zero Y")]
+        public void CircularCloudLayouter_GetsOnlyPositiveCenterCoordinates(int x, int y)
+        {
+            Action makeLayout = () => new CircularCloudLayouter(new Point(x, y));
+
+            makeLayout.Should().Throw<ArgumentException>()
+                .WithMessage("Center coordinates values have to be greater than Zero");
+        }
+
+        [Test]
+        public void PutNextRectangle_ShouldKeepEnteredSize()
+        {
+            var layout = new CircularCloudLayouter(new Point(5, 5));
+            var enteredSize = new Size(3, 4);
+            var returnedSize = layout.PutNextRectangle(enteredSize).Size;
+
+            returnedSize.Should().BeEquivalentTo(enteredSize);
+        }
+
+        //[Test]
+        //public void PutNextRectangle_ShouldMessageAboutLackOfFreeSpace()
+        //{
+        //    var layout = new CircularCloudLayouter(new Point(5, 5));
+
+        //    Action getTooBigRectangel = () => layout.PutNextRectangle(new Size(5, 11));
+        //    getTooBigRectangel.Should().Throw<ArgumentException>();
+        //}
+        //[Test]
+        //public void PutNextRectangle_ShouldPlaceItIntoLayoutBorders()
+        //{
+        //    var layout = new CircularCloudLayouter(new Point(5, 5));
+        //    var nextRectangleCentre = typeof(CircularCloudLayouter)
+        //        .GetField("nextRectangleCentre", BindingFlags.NonPublic | BindingFlags.Instance);
+        //    nextRectangleCentre.SetValue(layout, new Point(8,8));
+        //    var rectangle = layout.PutNextRectangle(new Size(3, 4));
+        //    var borders = new Rectangle(new Point(0, 0), layout.Size);
+
+        //    rectangle.IntersectsWith(borders).Should().BeTrue();
+        //}
+
+
+    }
+}
